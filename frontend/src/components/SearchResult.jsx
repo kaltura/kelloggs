c20102c766b4cd64929d4a523fafe1ab56cb3e47
kalturaguy <guy.jacubovski@kaltura.com>
2019-01-02 16:09:36 +0200

    slower refresh rate

--- frontend/src/components/SearchResult.jsx
+++ frontend/src/components/SearchResult.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -11,120 +11,129 @@ import {compose} from "recompose";
 import {withGlobalCommands} from "./GlobalCommands";
 
 const styles = {
-  root: {
-    position: 'relative',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    boxSizing: 'border-box'
-  },
-  loadingModal: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    width: '100px',
-    outline: 'none',
-    background: 'rgba(255, 255, 255, 0.9)',
-    borderRadius: '16px',
-    padding: '10px',
-    boxShadow: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    '& > .marginTop' : {
-      marginTop: '10px'
+    root: {
+        position: 'relative',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        boxSizing: 'border-box'
+    },
+    loadingModal: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100px',
+        outline: 'none',
+        background: 'rgba(255, 255, 255, 0.9)',
+        borderRadius: '16px',
+        padding: '10px',
+        boxShadow: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        '& > .marginTop': {
+            marginTop: '10px'
+        }
+    },
+    backdrop: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
     }
-  },
-  backdrop: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    position: 'absolute',
-    width: '100%',
-    height: '100%'
-  }
 };
 
 
 class SearchResult extends React.Component {
 
-  _loader = new ResultsLoader();
-  _cancelToken = null;
+    _loader = new ResultsLoader();
+    _cancel = false;
 
-  state = {
-    resultsData: null,
-    isProcessing: true,
-  }
+    state = {
+        resultsData: null,
+        isProcessing: true,
+    }
 
-  componentDidMount() {
-    const { parameters, globalCommands } = this.props;
-    const { serviceUrl, jwt } = globalCommands.config;
 
-    this._loader.loadUrl(serviceUrl, jwt, parameters);
+    componentDidMount() {
+        const {parameters, globalCommands} = this.props;
+        const {serviceUrl, jwt} = globalCommands.config;
 
-    let resultsData= null;
-    this._cancelToken = setInterval ( ()=> {
-      let queue= this._loader.popQueue();
-      if (queue.length>0) {
-        queue.forEach(element => {
-          if (!resultsData) {
-            resultsData = new ResultsData(element);
-            this.setState({
-              isProcessing: false,
-              resultsData:resultsData
-            });
+        this._loader.loadUrl(serviceUrl, jwt, parameters);
 
-          } else {
-            resultsData.append(element);
-          }
-        });
-        if (this._loader.completed) {
-            resultsData.setCompleted();
+        let resultsData = null;
+
+        let fetch = (time) => {
+
+            if (this._cancel) {
+                return;
+            }
+            try {
+                let queue = this._loader.popQueue();
+                if (queue.length > 0) {
+                    queue.forEach(element => {
+                        if (!resultsData) {
+                            resultsData = new ResultsData(element);
+                            this.setState({
+                                isProcessing: false,
+                                resultsData: resultsData
+                            });
+
+                        } else {
+                            resultsData.append(element);
+                        }
+                    });
+                    if (this._loader.completed) {
+                        resultsData.setCompleted();
+                    }
+                }
+            } catch (e) {
+
+            }
+            setTimeout(fetch, 2000)
         }
-      }
-    },100)
-  }
+        fetch(200);
+    }
 
-  componentWillUnmount() {
-      if (this._cancelToken) {
-        clearInterval(this._cancelToken);
-      }
+    componentWillUnmount() {
+        this._cancel = true;
+        this._loader.cancelLoading();
+    }
 
-    this._loader.cancelLoading();
-  }
-
-  render() {
-    const { classes, onClose} = this.props;
-    const { isProcessing, resultsData } = this.state;
+    render() {
+        const {classes, onClose} = this.props;
+        const {isProcessing, resultsData} = this.state;
 
 
-    return (
-      <div className={classes.root}>
-        { resultsData && <ResultsViewer results={resultsData}></ResultsViewer> }
-        {isProcessing &&
-        <React.Fragment>
-          <div className={classes.backdrop}></div>
-          <div className={classes.loadingModal}>
-            <Typography variant={'caption'}>Processing...</Typography>
-            <CircularProgress className={'marginTop'}/>
-            <Button onClick={onClose} variant={'text'} className={'marginTop'}>Abort</Button>
-          </div>
-        </React.Fragment>
-        }
-      </div>
+        return (
+            <div className={classes.root}>
+                {resultsData && <ResultsViewer results={resultsData}></ResultsViewer>}
+                {isProcessing &&
+                <React.Fragment>
+                    <div className={classes.backdrop}></div>
+                    <div className={classes.loadingModal}>
+                        <Typography variant={'caption'}>Processing...</Typography>
+                        <CircularProgress className={'marginTop'}/>
+                        <Button onClick={onClose} variant={'text'} className={'marginTop'}>Abort</Button>
+                    </div>
+                </React.Fragment>
+                }
+            </div>
 
-    )
-  }
+        )
+    }
 }
 
 
 SearchResult.propTypes = {
-  classes: PropTypes.object.isRequired,
-  globalCommands: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    globalCommands: PropTypes.object.isRequired
 };
 
 
 export default compose(
-  withStyles(styles),
-  withGlobalCommands
+    withStyles(styles),
+    withGlobalCommands
 )(SearchResult);
