6d1f9f4636c6cc1358e1bb460476f6e026c07f1b
Eran Sakal <eran@sakalim.com>
2019-02-18 10:38:14 +0200

    refact: make sure log types has valid value when switching from kms logs to api logs

--- frontend/src/components/parameters/APILogsParameters.jsx
+++ frontend/src/components/parameters/APILogsParameters.jsx
@@ -41,6 +41,16 @@ export default class APILogsParameters extends React.Component {
     return isValid;
   }
 
+  componentDidMount() {
+    const { onChange } = this.props;
+
+    if (['apiV3, ps2','apiV3', 'ps2','apiV3Analytics','accessLog'].indexOf(this.props.logTypes) !== -1) {
+      return;
+    }
+
+    onChange({ target : { name: 'logTypes', value: 'apiV3, ps2'}});
+  }
+
   render() {
     const { textFilter, session, server, logTypes, fromTime, toTime, onChange, onClear, className: classNameProp, onTextFilterChange } = this.props;
     const { isFromTimeValid, isToTimeValid } = this.state;
