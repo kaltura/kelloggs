e418e411905359f9c02448de099ff0a3485c3b5d
Omri Katz <omri.katz@kaltura.com>
2019-02-19 18:43:51 +0200

    run prettier

--- frontend/src/components/parameters/ObjectListParameters.jsx
+++ frontend/src/components/parameters/ObjectListParameters.jsx
@@ -1,50 +1,54 @@
-import React from 'react';
+import React from "react";
 import Grid from "@material-ui/core/Grid/Grid";
 import Paper from "@material-ui/core/Paper/Paper";
-import moment from 'moment';
+import moment from "moment";
 import FormControl from "@material-ui/core/FormControl/FormControl";
 import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import Select from "@material-ui/core/Select/Select";
 import Input from "@material-ui/core/Input/Input";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
-import ClearableTextField from '../ClearableTextField';
+import ClearableTextField from "../ClearableTextField";
 
-
-const FLAVOR_ASSET = {name: 'Flavor Asset', fields: ['entryIdIn', 'idIn', 'typeIn']};
-const BATCH_JOB_SEP = {name: 'Batch Job Sep' , fields: ['entryIdIn', 'objectIdIn', 'jobTypeIn']};
-const METADATA = {name: "Metadata", fields: ['objectIdIn', 'objectTypeIn']};
-const FILE_SYNC = {name: "File Sync", fields: ['objectIdIn', 'objectTypeIn']};
+const FLAVOR_ASSET = {
+  name: "Flavor Asset",
+  fields: ["entryIdIn", "idIn", "typeIn"]
+};
+const BATCH_JOB_SEP = {
+  name: "Batch Job Sep",
+  fields: ["entryIdIn", "objectIdIn", "jobTypeIn"]
+};
+const METADATA = { name: "Metadata", fields: ["objectIdIn", "objectTypeIn"] };
+const FILE_SYNC = { name: "File Sync", fields: ["objectIdIn", "objectTypeIn"] };
 
 const inputList = [
-    { name: "entryIdIn", label: "Entry ID in" },
-    { name: 'objectIdIn', label: 'Object ID in' },
-    { name: 'jobTypeIn', label: "Job Type in" },
-    { name: 'objectTypeIn', label: "Object Type in" },
-    { name: 'typeIn', label: "Type in" },
-    { name: 'idIn', label: "ID in" }
+  { name: "entryIdIn", label: "Entry ID in" },
+  { name: "objectIdIn", label: "Object ID in" },
+  { name: "jobTypeIn", label: "Job Type in" },
+  { name: "objectTypeIn", label: "Object Type in" },
+  { name: "typeIn", label: "Type in" },
+  { name: "idIn", label: "ID in" }
 ];
 const tableMap = new Map([
-    ['flavor_asset', FLAVOR_ASSET],
-    ['batch_job_sep', BATCH_JOB_SEP],
-    ['metadata', METADATA],
-    ['file_sync', FILE_SYNC]
+  ["flavor_asset", FLAVOR_ASSET],
+  ["batch_job_sep", BATCH_JOB_SEP],
+  ["metadata", METADATA],
+  ["file_sync", FILE_SYNC]
 ]);
 
-const findInput = name => inputList.filter(input => input.name === name)[0] || {};
+const findInput = name =>
+  inputList.filter(input => input.name === name)[0] || {};
 
 export default class ObjectListParameters extends React.Component {
   state = {
     isFromTimeValid: true,
     isToTimeValid: true
-  }
+  };
 
-  filterParameters = (parameters) => {
-
+  filterParameters = parameters => {
     const { table } = parameters;
     const { fields } = tableMap.get(table) || {};
 
     return Object.keys(parameters).reduce((acc, parameterName) => {
-
       if (fields.indexOf(parameterName) !== -1) {
         acc[parameterName] = parameters[parameterName];
       }
@@ -53,68 +57,71 @@ export default class ObjectListParameters extends React.Component {
   };
 
   validate = () => {
-    const isFromTimeValid = this._validateDate('fromTime', 'isFromTimeValid');
-    const isToTimeValid = this._validateDate('toTime', 'isToTimeValid');
+    const isFromTimeValid = this._validateDate("fromTime", "isFromTimeValid");
+    const isToTimeValid = this._validateDate("toTime", "isToTimeValid");
     return isFromTimeValid && isToTimeValid;
-  }
+  };
 
   _validateDate = (propertyName, validStateName) => {
     const value = this.props[propertyName];
-    const isValid = (value && moment(value).isValid());
+    const isValid = value && moment(value).isValid();
     this.setState({
       [validStateName]: isValid
-    })
+    });
 
     return isValid;
-  }
+  };
 
   _renderInputs = fields => {
-    const {onClear, onChange} = this.props;
+    const { onClear, onChange } = this.props;
     return fields.map(field => {
-          const {name, label} = findInput(field);
-          return (
-              <Grid item xs={4} key={name}>
-                  <ClearableTextField fullWidth
-                                      name={name}
-                                      label={label}
-                                      value={this.props[name]}
-                                      onClear={onClear}
-                                      onChange={onChange}
-                                      InputLabelProps={{
-                                          shrink: true,
-                                      }}
-                  />
-              </Grid>
-          )
-      })
-  }
+      const { name, label } = findInput(field);
+      return (
+        <Grid item xs={4} key={name}>
+          <ClearableTextField
+            fullWidth
+            name={name}
+            label={label}
+            value={this.props[name]}
+            onClear={onClear}
+            onChange={onChange}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+      );
+    });
+  };
 
   render() {
     const { table, onChange, onClear, className: classNameProp } = this.props;
     const fields = tableMap.get(table);
-    
+
     return (
       <Paper elevation={1} className={classNameProp}>
-        <Grid container spacing={16} >
+        <Grid container spacing={16}>
           <Grid item xs={4}>
-            <FormControl
-              fullWidth
-            >
+            <FormControl fullWidth>
               <InputLabel>Table</InputLabel>
               <Select
                 value={table}
                 onChange={onChange}
                 input={<Input name="table" id="type-input" />}
               >
-                  {Array.from(tableMap.keys()).map((fieldName, index)=> {
-                      return (<MenuItem key={index} value={fieldName}>{tableMap.get(fieldName).name}</MenuItem>)
-                  })}
+                {Array.from(tableMap.keys()).map((fieldName, index) => {
+                  return (
+                    <MenuItem key={index} value={fieldName}>
+                      {tableMap.get(fieldName).name}
+                    </MenuItem>
+                  );
+                })}
               </Select>
             </FormControl>
           </Grid>
-            {fields && this._renderInputs(fields.fields)}
+          {fields && this._renderInputs(fields.fields)}
         </Grid>
       </Paper>
-    )
+    );
   }
 }
